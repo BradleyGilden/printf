743ffaec69e0d39b233a836b1c33f2a009a59bb5
KevCareSA <kev.mkbn2@gmail.com>
2023-03-26 08:04:50 -0700

    Print chars

---
+++ print_chars.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_char - prints characters of integer.
+ *@ap: argument pointer.
+ *Return: 1 on success.
+ */
+
+int print_char(va_list ap)
+{
+	char c = va_arg(ap, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ *print_pcnt - prints percentages of integer.
+ *@ap: argument pointer.
+ *Return: 1 on success.
+ */
+
+int print_pcnt(va_list ap)
+{
+	(void)ap;
+
+	_putchar('%');
+	return (1);
+}
